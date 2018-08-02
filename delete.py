83177ef650f82bf1306b0e29cffc029c8c28767b
Nathan Yan <s-yann@bsd405.org>
2018-08-01 22:54:14 -0700

    Added ability to delete otps

---
+++ delete.py
@@ -0,0 +1,17 @@
+import click
+import keyring
+
+@click.command()
+@click.argument("name", required = True)
+def cli(name):
+    click.echo("Deleting a one-time password is permanent and irreversable. If this is your only copy of a one-time password, deleting it WILL lock you out of your account.")
+    delete = input("Delete your one-time password? [Y/n] ")
+
+    if (delete == 'y' or delete == 'Y'):
+        try:
+            keyring.delete_password("otp-cli", name.lower())
+        except:
+            click.echo("'%s' does not exist! Use 'otp write [name] [secret]' to create a new one-time password." % name)
+
+if __name__ == "__main__":
+    cli()
