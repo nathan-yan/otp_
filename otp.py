14289362c3ea63501a3abb6025a995d9ddc488bb
Nathan Yan <s-yann@bsd405.org>
2018-08-01 22:01:22 -0700

    OTP is split for readability

--- otp.py
+++ otp.py
@@ -9,10 +9,15 @@ valid_secret_pattern = re.compile(b"^([A-Z][2-7]+)+$")
 def validateSecret(secret):
     return valid_secret_pattern.match(secret.encode())
 
-def showCode(secret, totp = True, counter = None):
+def showCode(secret, totp = True, counter = None, split = True):
     if validateSecret(secret):
         return "Invalid secret! Valid secrets only contain the letters A-Z and digits 2-7"
     
     if (totp):
         generator = pyotp.TOTP(secret)
-        return generator.now()
+        code = generator.now()
+
+        if split:
+            return code[0: int(len(code) / 2)] + " " + code[int(len(code) / 2) : ]
+        else:
+            return code
