a9917b56ed372cc018848cc412b832b7f6c66d84
Nathan Yan <s-yann@bsd405.org>
2018-08-02 23:03:14 -0700

    Added details to setup

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 from setuptools import setup
 import sys
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
-if sys.platform != 'win32':
-    requirements.append('getch==1.0')
+requirements = [
+    "click>=6.7",
+    "pyotp>=2.2.6",
+    "keyring>=13.2.1",
+    "colorama>=0.3.9"
+]
 
 setup(
-    name = "otp",
-    version = "0.2",
-    py_modules = ["show", "otp", "getch_", "delete", "write", "show", "update"],
+    name = "otp-cli",
+    version = "0.4.3",
+	description = "A CLI one-time password application that manages one-time passwords elegantly and with tons of customization.", 
+	author = "Nathan Yan",
+	author_email = "nathancyan2002@gmail.com",
+	url = "https://github.com/nathan-yan/otp_",
+    py_modules = ["otp", "delete", "write", "show", "update", "getch_" ],
     install_requires = requirements,
     entry_points = '''
     [console_scripts]
